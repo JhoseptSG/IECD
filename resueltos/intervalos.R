e0d460782b964702184ff4fe938dcb39d03d9898
JhoseptSG <levyt22@gmail.com>
2023-09-21 13:38:08 -0300

    Arreglo errores

--- resueltos/intervalos.R
+++ resueltos/intervalos.R
@@ -1,25 +1,23 @@
-
 u = 4
 sigma = 3
 alfa = 0.95
-datos_normales = rnorm(n, mean = u, sd = sigma)
 
-IC_mu_var_conocida <- function(x,var_0,nivel) {
-  alfa_2 = 1 -nivel
-  a = qnorm(1-alfa_2/2)
+IC_mu_var_conocida <- function(x, var_0, nivel) {
+  alfa_2 = 1 - nivel
+  a = qnorm(1 - alfa_2 / 2)
   n = length(x)
-  res = c(mean(x) - a*sqrt(var_0/n) , mean(x) + a*sqrt(var_0/n))
+  res = c(mean(x) - a * sqrt(var_0 / n), mean(x) + a * sqrt(var_0 / n))
   return(res)
 }
 
 nrep = 1000
-muchos_intervalos = replicate(nrep,{
+muchos_intervalos = replicate(nrep, {
   datos_normales = rnorm(5, mean = u, sd = sigma)
-  IC_mu_var_conocida(datos_normales,sigma*sigma,alfa)
+  IC_mu_var_conocida(datos_normales, sigma^2, alfa)  # Usar nivel en lugar de alfa
   
 })
 
-sum((muchos_intervalos[1,]<=sigma) & (muchos_intervalos[2,]>=sigma))
+sum((muchos_intervalos[1,]<=u) & (muchos_intervalos[2,]>=u))
 
 
 IC_mu_var_desconocida <- function(x,nivel) {
@@ -28,7 +26,7 @@ IC_mu_var_desconocida <- function(x,nivel) {
   a = qt(1-alfa_2/2,n-1)
   se_hat = sqrt(var(x)/n)
   
-  length = length(datos_normales)
+  length = length(x)
   res = c(mean(x) - a*se_hat, mean(x) + a*se_hat)
   return(res)
 }
@@ -40,5 +38,25 @@ muchos_intervalos_desc = replicate(nrep,{
 })
 
 
-sum((muchos_intervalos_desc[1,]<=sigma) & (muchos_intervalos_desc[2,]>=sigma))
+sum((muchos_intervalos_desc[1,]<=u) & (muchos_intervalos_desc[2,]>=u))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
